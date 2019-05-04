04bf8e1cfa901b7973ad3ccda7895507b906f8fc
JayParanjape <jay.edutech@gmail.com>
2019-05-04 17:01:09 +0530

    Add menu

---
+++ Assets/scripts/main_menu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class main_menu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("game_scene");
+
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
