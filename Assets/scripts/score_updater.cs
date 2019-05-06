06fcd7f37c039378d1983e62892a10f4a418c58e
JayParanjape <jay.edutech@gmail.com>
2019-05-07 04:12:02 +0530

    Added btns, need to add scripts

---
+++ Assets/scripts/score_updater.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class score_updater : MonoBehaviour
+{
+    TextMesh.text = "High Score: " + PlayerPrefs.GetFloat("HighScore", 0).ToString("0");
+}
